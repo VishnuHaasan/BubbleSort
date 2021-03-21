15c44b8874ce0a40930a8c50a1829af359d4818d
Vishnu Haasan <vishnuhaasanoffl@gmail.com>
2021-03-21 12:00:16 +0530

    Files Added

---
+++ ruby.rb
@@ -0,0 +1,37 @@
+#Default sort by ascending order.
+def bubble_sort(array)
+  issorthappen = false;
+  for i in 1..array.length
+    issorthappen = false;
+    for j in 0..array.length-2
+      if array[j]>array[j+1]
+        temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+        issorthappen = true;
+      end;
+    end;
+    return array unless issorthappen;
+  end;
+  return array;
+end;
+#Sort by condition by passing a block as argument.
+def bubble_sort_by(array)
+  issorthappen = false;
+  for i in 1..array.length
+    issorthappen = false;
+    for j in 0..array.length-2
+      if (yield array[j],array[j+1]).to_i > 0
+        temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+        issorthappen = true;
+      end;
+    end;
+    return array unless issorthappen;
+  end;
+  return array;
+end;
+proc = Proc.new{|l,r| l <=> r};
+p bubble_sort([4,3,78,2,0,2]);
+p bubble_sort_by([4,3,78,2,0,2],&proc);
